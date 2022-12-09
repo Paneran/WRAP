4408e2c437d0eaf948b93beeaafd26849b391d98
Nathan Pereira <nathanxpereira@gmail.com>
2022-12-08 23:48:47 -0800

    little opt. of FFTs and clean up of code

--- C/complex.c
+++ C/complex.c
@@ -4,13 +4,13 @@
 #define M_PI 3.1415926535897932384
 
 int main() {
-    int ft = 0;
-    int cor = 1;
+    int ft = 1;
+    int cor = 0;
     if (cor) {
         double e[8] = {1, 2, 3, 4, 6, 9, 2, 4};
         double f[8] = {2, 3, 5, 7, 0, 2, 9, 5};
         double g[15];
-        xcorr(f, e, g, 8, 8, 7);
+        xcorr(f, e, g, 8, 8, 15);
         printf("g:\n");
         for (int i = 0; i < 15; i++) {
             printf("values of number: %.2lf\n", *(g+i));
@@ -32,14 +32,11 @@ int main() {
         double complex c[8]; 
         double complex d[8];
 
-
-
         FFTdp(a, A, 8);
         FFTdp(b, B, 2); 
         IFFTdp(A, c, 8);
         IFFTdp(B, d, 2);
 
-        
         printf("A:\n");
         for (int i = 0; i < 8; i++) {
             printf("values of complex number: %.2lf+%.2lfi\n", creal(*(A+i)), cimag(*(A+i)));
--- C/tools.c
+++ C/tools.c
@@ -9,7 +9,6 @@
 #define M_PI 3.1415926535897932384
 
 const int max_N = 2048;
-const int MAX_BUFFER = max_N*2 -1;
 
 // log2 copied from Stack Overflow, by Leos313
 int log2_(int N) {
@@ -37,33 +36,36 @@ void FFTdp(double complex* x, double complex* X, int N) {
     // Outside loop O(log(N))
     for (int i = 0; i < p; i++) {
         // two for loops below O(N) time
+        int b = N>>i;
+        int l = b>>1;
         for (int j = 0; j < (1<<i); j++) {
-            for (int k = 0; k < (N>>(i+1)); k++) {
-                *(X_e + k) = *(X + 2*k + j*(N>>i));
-                *(X_o + k) = *(X + 2*k + 1 + j*(N>>i));
+            for (int k = 0; k < l; k++) {
+                *(X_e + k) = *(X + 2*k + j*b);
+                *(X_o + k) = *(X + 2*k + 1 + j*b);
             }
-            for (int k = 0; k < (N>>(i+1)); k++) {
-                *(X + k + j*(N>>i)) = *(X_e + k);
-                *(X + k + j*(N>>i) + (N>>(i+1))) = *(X_o + k);
+            for (int k = 0; k < l; k++) {
+                *(X + k + j*b) = *(X_e + k);
+                *(X + k + j*b + l) = *(X_o + k);
             }    
         }
     }
     
-    // put together the 2 point FFT pairs to make whole FFT. 
+    // put together the 2 point FFT pairs to make whole FFT. O(N log(N)) 
     double complex w, tw;
     for (int i = 0; i < p; i++) {
-        int l = N>>(p-i); // half length of block
+        int l = 1<<i; // half block length
+        int b = l<<1; // full block length
         w = cexp(-I*2*M_PI/(l<<1));
         for (int j = 0; j < (N>>(i+1)); j++) { // iterate through each group
             for (int k = 0; k < l; k++) { // iterate through each point in each group
                 // first half of group is X_e and second half is X_o
-                *(X_e + k) = *(X + j*(1<<(i+1)) + k);
-                *(X_o + k) = *(X + j*(1<<(i+1)) + k + l);
+                *(X_e + k) = *(X + j*b + k);
+                *(X_o + k) = *(X + j*b + k + l);
             }
             tw = 1;
             for (int k = 0; k < l; k++) {
-                *(X + k + j*(1<<(i+1))) = *(X_e + k) + *(X_o + k) * tw;
-                *(X + k + j*(1<<(i+1)) + l) = *(X_e + k) - *(X_o + k) * tw;
+                *(X + k + j*b) = *(X_e + k) + *(X_o + k) * tw;
+                *(X + k + j*b + l) = *(X_e + k) - *(X_o + k) * tw;
                 tw *= w;
             }
         }
@@ -80,43 +82,44 @@ void IFFTdp(double complex* x, double complex* X, int N) {
         *(X + i) = *(x + i);
     }
     
-    // decompose x into its 2 point pairs for DFT. O(N log(N))
     int p = log2_(N);
-    // Outside loop O(log(N))
+    // decompose x into its 2 point pairs for IDFT. O(N log(N))
     for (int i = 0; i < p; i++) {
         // two for loops below O(N) time
+        int b = N>>i;
+        int l = b>>1;
         for (int j = 0; j < (1<<i); j++) {
-            for (int k = 0; k < (N>>(i+1)); k++) {
-                *(X_e + k) = *(X + 2*k + j*(N>>i));
-                *(X_o + k) = *(X + 2*k + 1 + j*(N>>i));
+            for (int k = 0; k < l; k++) {
+                *(X_e + k) = *(X + 2*k + j*b);
+                *(X_o + k) = *(X + 2*k + 1 + j*b);
             }
-            for (int k = 0; k < (N>>(i+1)); k++) {
-                *(X + k + j*(N>>i)) = *(X_e + k);
-                *(X + k + j*(N>>i) + (N>>(i+1))) = *(X_o + k);
+            for (int k = 0; k < l; k++) {
+                *(X + k + j*b) = *(X_e + k);
+                *(X + k + j*b + l) = *(X_o + k);
             }    
         }
     }
     
-    // put together the 2 point FFT pairs to make whole FFT. 
+    // put together the 2 point IDFT pairs to make whole IFFT. O(N log(N)) 
     double complex w, tw;
     for (int i = 0; i < p; i++) {
-        int l = N>>(p-i); // half length of block
+        int l = 1<<i; // half block length
+        int b = l<<1; // full block length
         w = cexp(I*2*M_PI/(l<<1));
-        double complex tw = 1;
         for (int j = 0; j < (N>>(i+1)); j++) { // iterate through each group
             for (int k = 0; k < l; k++) { // iterate through each point in each group
                 // first half of group is X_e and second half is X_o
-                *(X_e + k) = *(X + j*(1<<(i+1)) + k);
-                *(X_o + k) = *(X + j*(1<<(i+1)) + l + k);
+                *(X_e + k) = *(X + j*b + k);
+                *(X_o + k) = *(X + j*b + k + l);
             }
             tw = 1;
             for (int k = 0; k < l; k++) {
-                *(X + k + j*(1<<(i+1))) = *(X_e + k) + *(X_o + k) * tw;
-                *(X + k + j*(1<<(i+1)) + l) = *(X_e + k) - *(X_o + k) * tw;
+                *(X + k + j*b) = *(X_e + k) + *(X_o + k) * tw;
+                *(X + k + j*b + l) = *(X_e + k) - *(X_o + k) * tw;
                 tw *= w;
             }
         }
-    } 
+    }
 
     // divide each element by N
     for (int i = 0; i < N; i++) {
@@ -126,6 +129,7 @@ void IFFTdp(double complex* x, double complex* X, int N) {
 
 // crosscorrelation function
 void xcorr(double* x1, double* x2, double* X, int l1, int l2, int L) {
+    // preliminary warnings on output sizing. 
     if (L < (l1 + l2 -1)) {
         printf("WARNING: seg fault may occur. Make L=l1+l2-1\n");
         L = l1 + l2 - 1;
@@ -134,15 +138,18 @@ void xcorr(double* x1, double* x2, double* X, int l1, int l2, int L) {
         L = l1 + l2 - 1;
     }
     
+    // set initial pointer positions of what values in each vector should be considered
     double * a1 = x1 + l1 - 1, *a2 = x1 + l1 - 1, *b1 = x2, *b2 = x2;
-
-    for (int i = 0; i < L; i++) {
+    // set all values in X to zero
+    for (int i = 0; i < L; i++)
         *(X + i) = 0;
-    }
+    
+    // perform cross correlation
     int j;
     double * a, * b;
     for (int i = 1; i <= L; i++) {
         j = 0;
+        // finding the correlation at a certain index. 
         while(1) {
             a = a2 + j;
             b = b1 + j;
@@ -151,6 +158,7 @@ void xcorr(double* x1, double* x2, double* X, int l1, int l2, int L) {
                 break;
             j++;
         }
+        // conditional statements to move boundary pointers. 
         (i>= l2) ? (a1--) : (0);
         (i < l1) ? (a2--) : (0);
         (i>= l1) ? (b1++) : (0);
@@ -163,5 +171,11 @@ void complex2double(double complex* a, double * b) {
 }
 
 void double2complex(double complex* a, double * b) {
+    return;
+}
+
+// filter takes "a" and modifies it with b. 
+// have MATLAB generate the filters. 
+void filter(double * a, double * b, int l1, int l2) {
     return;
 }
